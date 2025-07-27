33b19d824370b72a809cddc7cfc53b280f9fd49a
FrogOnABike <mark.skrzypczyk.com>
2025-07-27 17:26:02 +0100

    start of cache implementation

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,16 +3,21 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/frogonabike/pokedexcli/internal/pokecache"
 )
 
 type Client struct {
-	httpClient http.Client
+	httpClient  http.Client
+	clientCache pokecache.Cache
 }
 
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout time.Duration, cacheTimeout time.Duration) Client {
+
 	return Client{
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
+		clientCache: pokecache.NewCache(cacheTimeout),
 	}
 }
---
+++ internal/pokecache/cache.go
@@ -0,0 +1,75 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	item     map[string]cacheEntry
+	mu       *sync.Mutex
+	interval time.Duration
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+// Create a new cache
+func NewCache(i time.Duration) Cache {
+	nc := Cache{
+		interval: i,
+	}
+	nc.reapLoop()
+	return nc
+}
+
+// Add entry to cache
+func (c Cache) Add(key string, data []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	ce := cacheEntry{
+		createdAt: time.Now(),
+		val:       data,
+	}
+	c.item[key] = ce
+}
+
+// Check for and fetch key from cache if found
+func (c Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	item, exists := c.item[key]
+	if exists {
+		return item.val, true
+	} else {
+		return nil, false
+	}
+}
+
+// Cache pruning - checks for entres older than cacheTimeout and removes then (Currently 5sec)
+func (c Cache) reapLoop() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	ticker := time.NewTicker(c.interval)
+	defer ticker.Stop()
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case <-ticker.C:
+				for k, i := range c.item {
+					itemAge := time.Since(i.createdAt)
+					if itemAge > c.interval {
+						delete(c.item, k)
+					}
+				}
+			}
+		}
+	}()
+
+}
